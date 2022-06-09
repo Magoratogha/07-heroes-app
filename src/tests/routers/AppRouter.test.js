98a9d99803b3822032ba7f40d6919c7668a1211f
Santiago Gómez Grajales <magoratoga@gmail.com>
2022-06-09 11:15:49 -0500

    Added more UTs for AppRouter

--- src/tests/routers/AppRouter.test.js
+++ src/tests/routers/AppRouter.test.js
@@ -4,17 +4,33 @@ import AppRouter from "../../routers/AppRouter";
 
 describe('Test on <AppRouter />', () => {
     
-    const contextValue = {
-        user: {
-            logged: false
-        }
-    };
-
     it('should render LoginScreen if user is not authenticated', () => {
+        const contextValue = {
+            user: {
+                logged: false
+            }
+        };
+        
         const { container } = render(<AuthContext.Provider value={ contextValue }>
                 <AppRouter />
             </AuthContext.Provider>);
         expect(container).toMatchSnapshot();
         expect(screen.getByText('Login Screen', { selector: 'h1' })).toBeInTheDocument();
     });
+    
+    it('should render Marvel component if user is authenticated', () => {
+        const contextValue = {
+            user: {
+                logged: true,
+                name: 'Test'
+            }
+        };
+        
+        const { container } = render(<AuthContext.Provider value={ contextValue }>
+                <AppRouter />
+            </AuthContext.Provider>);
+        expect(container).toMatchSnapshot();
+        expect(screen.getByText('Marvel Screen', { selector: 'h1' })).toBeInTheDocument();
+    });
+    
 });
